824d72f7205bdbd7296319bacf34a2ae16fe9bb3
eduardo01322 <eduardo.errea32@gmail.com>
2024-04-04 07:17:12 -0300

    .

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -20,4 +20,16 @@ class ProdutoController extends Controller
         });
         return response()->json($produtosComImagem);
     }
+    public function store(Request $request)
+    {
+        $produtoData = $request->all();
+        if ($request->hasFile('imagem')) {
+            $imagem = $request->file('imagem');
+            $nomeImagem = time() . '.' . $imagem->getClienteOriginalExtension();
+            $caminhoImagem = $imagem->storeAs('imagens/produtos', $nomeImagem, 'public');
+            $produtoData['imagem'] = $caminhoImagem;
+        }
+        $produto = Produto::create($produtoData);
+        return response()->json(['produto' => $produto], 201);
+    }
 }
